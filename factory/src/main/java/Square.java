2363d0e6ea15052d5ba7795ab93bb7b9bf54a3e4
Shashank Vivek <shashank.093028@gmail.com>
2017-06-20 14:41:33 +0530

    Factory Pattern adding files

---
+++ factory/src/main/java/Square.java
@@ -0,0 +1,9 @@
+/**
+ * Created by shashank on 20/6/17.
+ */
+public class Square implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Inside Square::draw() method.");
+    }
+}
