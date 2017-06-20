6c1a5619ced5113363c38232fe1a63dbe246d78a
Shashank Vivek <shashank.093028@gmail.com>
2017-06-20 14:41:10 +0530

    Factory Pattern.

---
+++ factory/src/main/java/Circle.java
@@ -0,0 +1,9 @@
+/**
+ * Created by shashank on 20/6/17.
+ */
+public class Circle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Inside Circle::draw() method.");
+    }
+}
